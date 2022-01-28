3091e8084a62863cfe5cf1d04af4229f669e1844
Raphael Bertholucci <raphaelbertholucci@gmail.com>
2022-01-28 12:56:51 -0300

    reponse generic function created :tada:

--- common/src/main/kotlin/com/bertholucci/common/extensions/LiveDataExtensions.kt
+++ common/src/main/kotlin/com/bertholucci/common/extensions/LiveDataExtensions.kt
@@ -2,6 +2,12 @@ package com.bertholucci.common.extensions
 
 import androidx.lifecycle.MutableLiveData
 import com.bertholucci.common.helpers.Response
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 
 fun <T : Any> MutableLiveData<T>.defaultValue(defaultValue: T, async: Boolean = false) =
     apply {
@@ -26,4 +32,16 @@ fun <V> MutableLiveData<Response<V>>.success(response: V) {
 
 fun <V> MutableLiveData<Response<V>>.failure(error: Throwable) {
     value = Response.Failure(error)
+}
+
+@ExperimentalCoroutinesApi
+fun <T : Any> Flow<T>.response(
+    liveData: MutableLiveData<Response<T>>,
+    map: (T) -> Response<T> = { Response.Success(it) }
+): Flow<Unit> {
+    return this
+        .onStart { liveData.showLoading() }
+        .map { liveData.value = map(it) }
+        .onCompletion { liveData.hideLoading() }
+        .catch { liveData.value = Response.Failure(it) }
 }
